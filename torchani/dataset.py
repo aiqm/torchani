b706854a1b42bed3fe2eb37db60a659258f242a2
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-05-10 11:13:13 -0700

    support save and load

--- torchani/dataset.py
+++ torchani/dataset.py
@@ -2,6 +2,8 @@ import h5py
 import os
 from .pyanitools import anidataloader
 import random
+import json
+import copy
 
 
 class Dataset:
@@ -11,16 +13,20 @@ class Dataset:
     different API.
     """
 
-    def __init__(self, filename):
-        """Initialize ANIDataset, from path to hdf5 files.
+    def __init__(self, filename=None):
+        """Initialize Dataset.
 
         Parameters
         ----------
         filename : string
             Path to hdf5 files. If a single file is specified, then all the data
             will come from this file. If a directory is specified, then all  files
-            with extension `.h5` or `.hdf5` in that directory will be loaded.
+            with extension `.h5` or `.hdf5` in that directory will be loaded. If
+            `None` is given, then the dataset will not be initialized. The user
+            then have to manually load from a json file dump.
         """
+        if filename is None:
+            return
         if os.path.isdir(filename):
             complete_filenames = [os.path.join(
                 filename, f) for f in os.listdir(filename)]
@@ -30,8 +36,11 @@ class Dataset:
             filenames = [filename]
         else:
             raise ValueError('Bad file name')
-        h5files = [anidataloader(f) for f in filenames]
-        self._keys = [(f, i['path'])for f in h5files for i in f]
+        self._loaders = {}
+        for f in filenames:
+            self._loaders[f] = anidataloader(f)
+        self._keys = [(f, i['path'])
+                      for f in filenames for i in self._loaders[f]]
         self._subsets = {}
 
     def shuffle(self):
@@ -52,13 +61,14 @@ class Dataset:
         ----------
         *subsets : list of (string, float)
             List of names and ratios of subsets. Names must be different. All ratios
-            must sum to 1.
+            must sum to 1. Name of sublist can not be 'all', which is reserved for the
+            whole dataset.
         """
         self._subsets = {}
         total_ratio = 0
         start_index = 0
         for name, ratio in subsets:
-            if name in self._subsets:
+            if name in self._subsets or name == 'all':
                 raise ValueError('duplicate names')
             total_ratio += ratio
             end_index = int(total_ratio * len(self._keys))
@@ -66,6 +76,31 @@ class Dataset:
             start_index = end_index
         if total_ratio != 1:
             raise ValueError('ratio must sum to 1')
+
+    def save(self, filename):
+        """Save the current shuffle and splits into file"""
+        d = copy.copy(self._subsets)
+        d['all'] = self._keys
+        d = json.dumps(d)
+        with open(filename, 'w') as f:
+            f.write(d)
+
+    def load(self, filename):
+        """Load shuffle and splits from file"""
+        # read keys and subsets
+        with open(filename, 'r') as f:
+            d = json.loads(f.read())
+            self._keys = [(x[0], x[1]) for x in d['all']]
+            del d['all']
+            self._subsets = {}
+            for k in d:
+                self._subsets[k] = [(x[0], x[1]) for x in d[k]]
+            self._loaders = {}
+
+        # initialize loaders
+        for f, _ in self._keys:
+            if f not in self._loaders:
+                self._loaders[f] = anidataloader(f)
 
     def iter(self, batch_size, subset=None):
         """Iterate through the selected subset. Each yield get batch size number of conformations
@@ -78,10 +113,13 @@ class Dataset:
         batch_size : int
             Number of conformations to yield each time.
         subset : string
-            The name of subset to iterate. If `None` is given, then iterate on the whole dataset.
+            The name of subset to iterate. If `None` or 'all' is given, then iterate on the
+            whole dataset.
         """
-        subset = self._keys if subset is None else self._subsets[subset]
-        for loader, path in subset:
+        subset = self._keys if (
+            subset is None or subset == 'all') else self._subsets[subset]
+        for filename, path in subset:
+            loader = self._loaders[filename]
             data = loader.store[path]
             species = [i.decode('ascii') for i in data['species']]
             coordinates = data['coordinates'][()]
