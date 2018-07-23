acee2d4d4c7ad6ace257cf5877bf466d9be021c7
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-23 17:19:05 +0800

    add unit test for model ensemble (#14)

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -2,9 +2,10 @@ from .energyshifter import EnergyShifter
 from .nn import ModelOnAEV, PerSpeciesFromNeuroChem
 from .aev import SortedAEV
 from .env import buildin_const_file, buildin_sae_file, buildin_network_dir, \
-    buildin_model_prefix, default_dtype, default_device
+    buildin_model_prefix, buildin_ensembles, default_dtype, default_device
 
 __all__ = ['SortedAEV', 'EnergyShifter', 'ModelOnAEV',
            'PerSpeciesFromNeuroChem', 'data', 'buildin_const_file',
            'buildin_sae_file', 'buildin_network_dir', 'buildin_dataset_dir',
-           'buildin_model_prefix', 'default_dtype', 'default_device']
+           'buildin_model_prefix', 'buildin_ensembles', 'default_dtype',
+           'default_device']
