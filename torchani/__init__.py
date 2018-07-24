aa3d34d3ff50bcd31705c897d565b6c49b9028b9
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-24 10:44:49 +0800

    fix dataset test

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -3,9 +3,9 @@ from .nn import ModelOnAEV, PerSpeciesFromNeuroChem
 from .aev import SortedAEV
 from .env import buildin_const_file, buildin_sae_file, buildin_network_dir, \
     buildin_model_prefix, buildin_ensembles, default_dtype, default_device
+from .data import ANIDataset
 
 __all__ = ['SortedAEV', 'EnergyShifter', 'ModelOnAEV',
-           'PerSpeciesFromNeuroChem', 'data', 'buildin_const_file',
-           'buildin_sae_file', 'buildin_network_dir', 'buildin_dataset_dir',
-           'buildin_model_prefix', 'buildin_ensembles', 'default_dtype',
-           'default_device']
+           'PerSpeciesFromNeuroChem', 'ANIDataset', 'buildin_const_file',
+           'buildin_sae_file', 'buildin_network_dir', 'buildin_model_prefix',
+           'buildin_ensembles', 'default_dtype', 'default_device']
