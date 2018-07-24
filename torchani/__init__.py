27997ba27f44a463c2dd6b0d8044325c556378a7
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-24 11:00:52 +0800

    try fixing for python2

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -3,9 +3,8 @@ from .nn import ModelOnAEV, PerSpeciesFromNeuroChem
 from .aev import SortedAEV
 from .env import buildin_const_file, buildin_sae_file, buildin_network_dir, \
     buildin_model_prefix, buildin_ensembles, default_dtype, default_device
-from .data import ANIDataset
 
 __all__ = ['SortedAEV', 'EnergyShifter', 'ModelOnAEV',
-           'PerSpeciesFromNeuroChem', 'ANIDataset', 'buildin_const_file',
+           'PerSpeciesFromNeuroChem', 'data', 'buildin_const_file',
            'buildin_sae_file', 'buildin_network_dir', 'buildin_model_prefix',
            'buildin_ensembles', 'default_dtype', 'default_device']
