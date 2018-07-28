965efee230d27cb2959bee5f074198663cc218b4
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-28 14:13:59 -0400

    helper function to create dataloader, helper module that handle batch (#27)

--- torchani/models/__init__.py
+++ torchani/models/__init__.py
@@ -1,4 +1,5 @@
 from .custom import CustomModel
 from .neurochem_nnp import NeuroChemNNP
+from .batch import BatchModel
 
-__all__ = ['CustomModel', 'NeuroChemNNP']
+__all__ = ['CustomModel', 'NeuroChemNNP', 'BatchModel']
