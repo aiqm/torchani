46e784d293ecef1534fba19d10de80c220ffbb7c
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-28 13:00:38 -0400

    batch

---
+++ tests/test_batch.py
@@ -0,0 +1,31 @@
+import sys
+
+if sys.version_info.major >= 3:
+    import os
+    import unittest
+    import torch
+    import torchani
+    import torchani.data
+
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(path, 'dataset')
+    chunksize = 32
+    batch_chunks = 32
+    dtype = torch.float32
+    device = torch.device('cpu')
+
+    class TestBatch(unittest.TestCase):
+
+        def testBatchLoadAndInference(self):
+            ds = torchani.data.ANIDataset(path, chunksize)
+            loader = torchani.data.dataloader(ds, batch_chunks)
+            aev_computer = torchani.SortedAEV(dtype=dtype, device=device)
+            nnp = torchani.models.NeuroChemNNP(aev_computer)
+            batch_nnp = torchani.models.BatchModel(nnp)
+            for batch_input, batch_output in loader:
+                batch_output_ = batch_nnp(batch_input)
+                self.assertListEqual(batch_output_.shape,
+                                     batch_output['energies'])
+
+    if __name__ == '__main__':
+        unittest.main()
