71d84b9e1db6ca9aea146103d09bff02ac82feb2
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-29 12:24:49 -0400

    add ignite API helpers

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -1,10 +1,12 @@
 from .energyshifter import EnergyShifter
 from . import models
+from . import data
+from . import ignite
 from .aev import SortedAEV
 from .env import buildin_const_file, buildin_sae_file, buildin_network_dir, \
     buildin_model_prefix, buildin_ensemble, default_dtype, default_device
 
-__all__ = ['SortedAEV', 'EnergyShifter', 'models', 'data',
+__all__ = ['SortedAEV', 'EnergyShifter', 'models', 'data', 'ignite',
            'buildin_const_file', 'buildin_sae_file', 'buildin_network_dir',
            'buildin_model_prefix', 'buildin_ensemble',
            'default_dtype', 'default_device']
