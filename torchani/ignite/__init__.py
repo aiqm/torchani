af13125881692a94876e1e5e92ec2011180706fa
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-30 19:23:48 -0400

    add common loss and metrics

--- torchani/ignite/__init__.py
+++ torchani/ignite/__init__.py
@@ -1,4 +1,6 @@
 from .container import Container
-from .loss_metrics import DictLoss, DictMetric
+from .loss_metrics import DictLoss, DictMetric, energy_mse_loss, \
+    energy_rmse_metric
 
-__all__ = ['Container', 'DictLoss', 'DictMetric']
+__all__ = ['Container', 'DictLoss', 'DictMetric', 'energy_mse_loss',
+           'energy_rmse_metric']
