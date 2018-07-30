f7289c930fab978f8c8fc241d4d987dc3a34991f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-30 19:41:15 -0400

    simple examples

--- examples/nnp_training.py
+++ examples/nnp_training.py
@@ -38,8 +38,31 @@ metric = torchani.ignite.DictMetric('energies',
                                     ignite.metrics.RootMeanSquaredError())
 optimizer = torch.optim.Adam(nnp.parameters())
 trainer = ignite.engine.create_supervised_trainer(container, optimizer, loss)
-validator = ignite.engine.create_supervised_evaluator(container, metrics={
+evaluator = ignite.engine.create_supervised_evaluator(container, metrics={
         'RMSE': metric
     })
+
+
+@trainer.on(ignite.engine.Events.ITERATION_COMPLETED)
+def log_training_loss(trainer):
+    print("Epoch[{}] Loss: {:.2f}".format(trainer.state.epoch,
+                                          trainer.state.output))
+
+
+@trainer.on(ignite.engine.Events.EPOCH_COMPLETED)
+def log_training_results(trainer):
+    evaluator.run(training)
+    metrics = evaluator.state.metrics
+    print("Training Results - Epoch: {}  RMSE: {:.2f}"
+          .format(trainer.state.epoch, metrics['RMSE']))
+
+
+@trainer.on(ignite.engine.Events.EPOCH_COMPLETED)
+def log_validation_results(validation):
+    evaluator.run(validation)
+    metrics = evaluator.state.metrics
+    print("Validation Results - Epoch: {}  RMSE: {:.2f}"
+          .format(trainer.state.epoch, metrics['RMSE']))
+
 
 trainer.run(training, max_epochs=100)
