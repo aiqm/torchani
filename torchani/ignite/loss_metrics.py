e2bc9f292ecd20da03f86a8117978ba4e144c14e
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-30 15:39:17 -0400

    add DictMetric (#31)

---
+++ torchani/ignite/loss_metrics.py
@@ -0,0 +1,31 @@
+from torch.nn.modules.loss import _Loss
+from ignite.metrics.metric import Metric
+
+
+class DictLoss(_Loss):
+
+    def __init__(self, key, loss):
+        super(DictLoss, self).__init__()
+        self.key = key
+        self.loss = loss
+
+    def forward(self, input, other):
+        return self.loss(input[self.key], other[self.key])
+
+
+class DictMetric(Metric):
+
+    def __init__(self, key, metric):
+        self.key = key
+        self.metric = metric
+        super(DictMetric, self).__init__()
+
+    def reset(self):
+        self.metric.reset()
+
+    def update(self, output):
+        y_pred, y = output
+        self.metric.update((y_pred[self.key], y[self.key]))
+
+    def compute(self):
+        self.metric.compute()
