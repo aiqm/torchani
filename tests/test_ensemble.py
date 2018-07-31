c94c1ae083fcaf7a70a0bacac60ad76c54d5f098
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-07-31 15:01:06 -0400

    remove force from ANIModel

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -15,21 +15,21 @@ class TestEnsemble(unittest.TestCase):
         self.conformations = 20
 
     def _test_molecule(self, coordinates, species):
+        coordinates = torch.tensor(coordinates, requires_grad=True)
         n = torchani.buildin_ensemble
         prefix = torchani.buildin_model_prefix
         aev = torchani.SortedAEV(device=torch.device('cpu'))
-        ensemble = torchani.models.NeuroChemNNP(aev, derivative=True,
-                                                ensemble=True)
+        ensemble = torchani.models.NeuroChemNNP(aev, ensemble=True)
         models = [torchani.models.
-                  NeuroChemNNP(aev, derivative=True,
-                               ensemble=False,
+                  NeuroChemNNP(aev, ensemble=False,
                                from_=prefix + '{}/networks/'.format(i))
                   for i in range(n)]
 
-        energy1, force1 = ensemble(coordinates, species)
-        energy2, force2 = zip(*[m(coordinates, species) for m in models])
+        energy1 = ensemble(coordinates, species)
+        force1 = torch.autograd.grad(energy1.sum(), coordinates)[0]
+        energy2 = [m(coordinates, species) for m in models]
         energy2 = sum(energy2) / n
-        force2 = sum(force2) / n
+        force2 = torch.autograd.grad(energy2.sum(), coordinates)[0]
         energy_diff = (energy1 - energy2).abs().max().item()
         force_diff = (force1 - force2).abs().max().item()
         self.assertLess(energy_diff, self.tol)
