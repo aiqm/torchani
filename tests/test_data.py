d3fe36376b880f502eacc071e53aaaabf48b6310
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-08-11 10:12:15 -0400

    pad_and_batch

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,31 +1,109 @@
 import sys
+import os
+import unittest
+import torch
+import torchani.data
 
-if sys.version_info.major >= 3:
-    import os
-    import unittest
-    import torchani.data
+path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(path, '../dataset')
 
-    path = os.path.dirname(os.path.realpath(__file__))
-    path = os.path.join(path, '../dataset')
+class TestDataset(unittest.TestCase):
 
-    class TestDataset(unittest.TestCase):
+    def _test_chunksize(self, chunksize):
+        ds = torchani.data.ANIDataset(path, chunksize)
+        for i, _ in ds:
+            self.assertLessEqual(i['coordinates'].shape[0], chunksize)
 
-        def _test_chunksize(self, chunksize):
-            ds = torchani.data.ANIDataset(path, chunksize)
-            for i, _ in ds:
-                self.assertLessEqual(i['coordinates'].shape[0], chunksize)
+    def testChunk64(self):
+        self._test_chunksize(64)
 
-        def testChunk64(self):
-            self._test_chunksize(64)
+    def testChunk128(self):
+        self._test_chunksize(128)
 
-        def testChunk128(self):
-            self._test_chunksize(128)
+    def testChunk32(self):
+        self._test_chunksize(32)
 
-        def testChunk32(self):
-            self._test_chunksize(32)
+    def testChunk256(self):
+        self._test_chunksize(256)
 
-        def testChunk256(self):
-            self._test_chunksize(256)
 
-    if __name__ == '__main__':
-        unittest.main()
+class TestPadAndBatch(unittest.TestCase):
+
+    def testVectorSpecies(self):
+        species1 = torch.LongTensor([0, 2, 3, 1])
+        coordinates1 = torch.zeros(5, 4, 3)
+        species2 = torch.LongTensor([3, 2, 0, 1, 0])
+        coordinates2 = torch.zeros(2, 5, 3)
+        species, coordinates = torchani.data.pad_and_batch([
+            (species1, coordinates1),
+            (species2, coordinates2),
+        ])
+        self.assertEqual(species.shape[0], 7)
+        self.assertEqual(species.shape[0], 5)
+        expected_species = torch.LongTensor([
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [3, 2, 0, 1, 0],
+            [3, 2, 0, 1, 0],
+        ])
+        self.assertEqual((species - expected_species).abs().max().item(), 0)
+        self.assertEqual(coordinates.abs().max().item(), 0)
+
+    def testTensorShape1NSpecies(self):
+        species1 = torch.LongTensor([[0, 2, 3, 1]])
+        coordinates1 = torch.zeros(5, 4, 3)
+        species2 = torch.LongTensor([3, 2, 0, 1, 0])
+        coordinates2 = torch.zeros(2, 5, 3)
+        species, coordinates = torchani.data.pad_and_batch([
+            (species1, coordinates1),
+            (species2, coordinates2),
+        ])
+        self.assertEqual(species.shape[0], 7)
+        self.assertEqual(species.shape[0], 5)
+        expected_species = torch.LongTensor([
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [3, 2, 0, 1, 0],
+            [3, 2, 0, 1, 0],
+        ])
+        self.assertEqual((species - expected_species).abs().max().item(), 0)
+        self.assertEqual(coordinates.abs().max().item(), 0)
+
+    def testTensorSpecies(self):
+        species1 = torch.LongTensor([
+            [0, 2, 3, 1],
+            [0, 2, 3, 1],
+            [0, 2, 3, 1],
+            [0, 2, 3, 1],
+            [0, 2, 3, 1],
+        ])
+        coordinates1 = torch.zeros(5, 4, 3)
+        species2 = torch.LongTensor([3, 2, 0, 1, 0])
+        coordinates2 = torch.zeros(2, 5, 3)
+        species, coordinates = torchani.data.pad_and_batch([
+            (species1, coordinates1),
+            (species2, coordinates2),
+        ])
+        self.assertEqual(species.shape[0], 7)
+        self.assertEqual(species.shape[0], 5)
+        expected_species = torch.LongTensor([
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [0, 2, 3, 1, -1],
+            [3, 2, 0, 1, 0],
+            [3, 2, 0, 1, 0],
+        ])
+        self.assertEqual((species - expected_species).abs().max().item(), 0)
+        self.assertEqual(coordinates.abs().max().item(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- torchani/data.py
+++ torchani/data.py
@@ -110,3 +110,21 @@ def collate(batch):
 def dataloader(dataset, batch_chunks, shuffle=True, **kwargs):
     return DataLoader(dataset, batch_chunks, shuffle,
                       collate_fn=collate, **kwargs)
+
+
+def pad_and_batch(species_coordinates):
+    max_atoms = max([c.shape[1] for _, c in species_coordinates])
+    species = []
+    coordinates = []
+    for s, c in species_coordinates:
+        natoms = c.shape[1]
+        if len(s.shape) == 1:
+            s = s.unsqueeze(0)
+        if natoms < max_atoms:
+            padding = torch.full((s.shape[0], max_atoms - natoms), -1,
+                                    dtype=torch.long, device=s.device)
+            s = torch.cat([s, padding])
+        s = s.expand(c.shape[0], max_atoms)
+        species.append(s)
+        coordinates.append(c)
+    return torch.cat(species), torch.cat(coordinates)
