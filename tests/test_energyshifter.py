1aaa87bfec8f15b53285142023d6fb91b3d6bf00
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-08-11 12:33:01 -0400

    improve energy shift

--- tests/test_energyshifter.py
+++ tests/test_energyshifter.py
@@ -7,21 +7,25 @@ import random
 class TestEnergyShifter(unittest.TestCase):
 
     def setUp(self):
-        self.tol = 5e-5
+        self.tol = 1e-5
         self.species = torchani.AEVComputer().species
         self.prepare = torchani.PrepareInput(self.species)
         self.shift_energy = torchani.EnergyShifter(self.species)
 
     def testSAEMatch(self):
+        species_coordinates = []
+        saes = []
         for _ in range(10):
             k = random.choice(range(5, 30))
             species = random.choices(self.species, k=k)
-            species_tensor = self.prepare.species_to_tensor(
-                species, torch.device('cpu'))
-            e1 = self.shift_energy.sae_from_list(species)
-            e2 = self.shift_energy.sae_from_tensor(
-                species_tensor.unsqueeze(0)).item()
-            self.assertLess(abs(e1 - e2), self.tol)
+            coordinates = torch.empty(1, k, 3)
+            species_coordinates.append(self.prepare((species, coordinates)))
+            e = self.shift_energy.sae_from_list(species)
+            saes.append(e)
+        species, _ = torchani.data.pad_and_batch(species_coordinates)
+        saes_ = self.shift_energy.sae_from_tensor(species)
+        saes = torch.tensor(saes, dtype=saes_.dtype, device=saes_.device)
+        self.assertLess((saes - saes_).abs().max(), self.tol)
 
 
 if __name__ == '__main__':
--- torchani/energyshifter.py
+++ torchani/energyshifter.py
@@ -19,14 +19,17 @@ class EnergyShifter(torch.nn.Module):
                     pass  # ignore unrecognizable line
         self_energies_tensor = [self.self_energies[s] for s in species]
         self.register_buffer('self_energies_tensor',
-                             torch.tensor(self_energies_tensor))
+                             torch.tensor(self_energies_tensor,
+                                          dtype=torch.double))
 
     def sae_from_list(self, species):
         energies = [self.self_energies[i] for i in species]
         return sum(energies)
 
     def sae_from_tensor(self, species):
-        return self.self_energies_tensor[species].sum(dim=1)
+        self_energies = self.self_energies_tensor[species]
+        self_energies[species==-1] = 0
+        return self_energies.sum(dim=1)
 
     def subtract_from_dataset(self, data):
         sae = self.sae_from_list(data['species'])
@@ -35,5 +38,5 @@ class EnergyShifter(torch.nn.Module):
 
     def forward(self, species_energies):
         species, energies = species_energies
-        sae = self.sae_from_tensor(species)
+        sae = self.sae_from_tensor(species).to(energies.dtype)
         return species, energies + sae
