571972ba88998601d2da284d4a4e8451888657ba
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-08-12 20:40:08 -0700

    fix import

--- torchani/training/__init__.py
+++ torchani/training/__init__.py
@@ -2,7 +2,7 @@ from .container import Container
 from .data import BatchedANIDataset, load_or_create
 from .loss_metrics import DictLoss, DictMetric, MSELoss, RMSEMetric, \
     TransformedLoss
-import .pyanitools
+from . import pyanitools
 
 __all__ = ['Container', 'BatchedANIDataset', 'load_or_create', 'DictLoss',
            'DictMetric', 'MSELoss', 'RMSEMetric', 'TransformedLoss',
