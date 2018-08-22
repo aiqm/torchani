997c02eb7931cfbbc79055ce102407e76986dfe1
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-08-22 12:21:46 -0400

    unify training

--- torchani/training/__init__.py
+++
@@ -1,6 +0,0 @@
-from .container import Container
-from .loss_metrics import DictLoss, DictMetric, MSELoss, RMSEMetric, \
-    TransformedLoss
-
-__all__ = ['Container', 'DictLoss',
-           'DictMetric', 'MSELoss', 'RMSEMetric', 'TransformedLoss']
