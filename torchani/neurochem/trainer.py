89104b0ee4cc0b5bf5d14fc13af4dd73c6f5db7a
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-09-10 21:27:30 -0400

    allow aev cache in command line

--- torchani/neurochem/trainer.py
+++ torchani/neurochem/trainer.py
@@ -26,9 +26,12 @@ if __name__ == '__main__':
     parser.add_argument('--tensorboard',
                         help='Directory to store tensorboard log files',
                         default=None)
+    parser.add_argument('--cache-aev', dest='cache_aev', action='store_true',
+                        help='Whether to cache AEV', default=None)
     parser = parser.parse_args()
 
     d = torch.device(parser.device)
-    trainer = Trainer(parser.config_path, d, parser.tqdm, parser.tensorboard)
+    trainer = Trainer(parser.config_path, d, parser.tqdm, parser.tensorboard,
+                      parser.cache_aev)
     trainer.load_data(parser.training_path, parser.validation_path)
     trainer.run()
