ed48e5f3dc3b566dcb2e9970664bca8db741afd3
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-10-29 14:26:02 -0400

    rename

--- examples/ase_interface.py
+++ examples/ase_interface.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Constant temperature MD using ASE interface
-===========================================
+Structure minimization and constant temperature MD using ASE interface
+======================================================================
 
-This example is modified from the official `Constant temperature MD`_ to use
-the ASE interface of TorchANI as energy calculator.
+This example is modified from the official `home page` and
+`Constant temperature MD`_ to use the ASE interface of TorchANI as energy
+calculator.
 
+.. _home page:
+    https://wiki.fysik.dtu.dk/ase/
 .. _Constant temperature MD:
     https://wiki.fysik.dtu.dk/ase/tutorials/md/md.html#constant-temperature-md
 """
@@ -15,6 +18,7 @@ the ASE interface of TorchANI as energy calculator.
 # To begin with, let's first import the modules we will use:
 from ase.lattice.cubic import Diamond
 from ase.md.langevin import Langevin
+from ase.optimize import BFGS
 from ase import units
 import torchani
 
@@ -22,6 +26,7 @@ import torchani
 ###############################################################################
 # Now let's set up a crystal
 atoms = Diamond(symbol="C", pbc=True)
+print(len(atoms), "atoms in the cell")
 
 ###############################################################################
 # Now let's create a calculator from builtin models:
@@ -30,16 +35,10 @@ calculator = torchani.ase.Calculator(builtin.species, builtin.aev_computer,
                                      builtin.models, builtin.energy_shifter)
 atoms.set_calculator(calculator)
 
-###############################################################################
-# We want to run MD with constant energy using the Langevin algorithm
-# with a time step of 5 fs, the temperature 50K and the friction
-# coefficient to 0.02 atomic units.
-dyn = Langevin(atoms, 5 * units.fs, 50 * units.kB, 0.002)
-
 
 ###############################################################################
-# Let's print energies every 50 steps:
-def printenergy(a=atoms):  # store a reference to atoms in the definition.
+# Now create a callback function that print interesting physical quantities:
+def printenergy(a=atoms):
     """Function to print the potential, kinetic and total energy."""
     epot = a.get_potential_energy() / len(a)
     ekin = a.get_kinetic_energy() / len(a)
@@ -47,9 +46,22 @@ def printenergy(a=atoms):  # store a reference to atoms in the definition.
           'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
 
 
-dyn.attach(printenergy, interval=50)
+###############################################################################
+# Now let's minimize the structure:
+printenergy()
+opt = BFGS(atoms)
+opt.attach(printenergy, interval=50)
+opt.run(5000)
+
+###############################################################################
+# We want to run MD with constant energy using the Langevin algorithm
+# with a time step of 5 fs, the temperature 50K and the friction
+# coefficient to 0.02 atomic units.
+dyn = Langevin(atoms, 1 * units.fs, 50 * units.kB, 0.2)
+
+dyn.attach(printenergy, interval=1)
 
 ###############################################################################
 # Now run the dynamics:
 printenergy()
-dyn.run(500)
+dyn.run(5000)
