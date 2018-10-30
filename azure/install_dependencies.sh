3cc546cfb97298473208504c3ecc124076f741e2
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-10-30 00:50:35 -0400

    more deps

--- azure/install_dependencies.sh
+++ azure/install_dependencies.sh
@@ -2,4 +2,5 @@
 
 python -m pip install --upgrade pip
 pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+pip install tqdm ase tensorboardX
 pip install pytorch-ignite --no-deps
