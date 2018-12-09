0bdb566d5189f8e8220f90f78b9a54f4be04eb8d
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-12-09 00:53:42 -0500

    Add dependency of pytorch and ignite back (#145)
    * fix lfs
    
    * dependency on  pytorch-ignite-nightly

--- azure/install_dependencies.sh
+++ azure/install_dependencies.sh
@@ -3,4 +3,3 @@
 python -m pip install --upgrade pip
 pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 pip install tqdm ase tensorboardX pyyaml
-pip install pytorch-ignite --no-deps
