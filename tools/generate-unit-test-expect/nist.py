035bce1d6a64d20c61f451012f358c781eee7a0b
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-12-12 14:37:58 -0500

    reorganize generate-unit-test-expect

--- tools/generate-unit-test-expect/nist.py
+++ tools/generate-unit-test-expect/nist.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import pyanitools
 import numpy
 from neurochem_calculator import NeuroChem, path
 import json
@@ -9,24 +8,6 @@ import random
 
 
 neurochem = NeuroChem()
-
-# generate expect for ANI1 subset
-mol_count = 0
-for i in [1, 2, 3, 4]:
-    data_file = os.path.join(
-        path, '../dataset/ani_gdb_s0{}.h5'.format(i))
-    adl = pyanitools.anidataloader(data_file)
-    for data in tqdm.tqdm(adl, desc='ANI1: {} heavy atoms'.format(i)):
-        coordinates = data['coordinates'][:10, :]
-        pickleobj = neurochem(coordinates, data['species'])
-        dumpfile = os.path.join(
-            path, '../tests/test_data/ANI1_subset/{}'.format(mol_count))
-        with open(dumpfile, 'wb') as f:
-            pickle.dump(pickleobj, f)
-        mol_count += 1
-
-
-# generate expect for NIST
 keep_ratio = 0.1  # reduce the size of generated file by discarding
 mol_count = 0
 with open(os.path.join(path, 'diverse_test_set/result.json')) as f:
