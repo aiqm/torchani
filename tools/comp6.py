aa35d51155a417614d994e1bf3147bfe6a4f6ccb
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-03-22 16:58:37 -0400

    fix

--- tools/comp6.py
+++ tools/comp6.py
@@ -8,19 +8,21 @@ import tqdm
 
 
 HARTREE2KCAL = 627.509
+dtype = torch.float32
 
 # parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', help='Path to the COMP6 directory')
-parser.add_argument('-b', '--batchsize', type=int, default=2048)
+parser.add_argument('-b', '--batchatoms', type=int, default=512,
+                    help='Maximum number of ATOMs in each batch')
 parser.add_argument('-d', '--device',
                     help='Device of modules and tensors',
                     default=('cuda' if torch.cuda.is_available() else 'cpu'))
 parser = parser.parse_args()
 
 # run benchmark
-ani1x = torchani.models.ANI1x().to(torch.float64).to(parser.device)
-ani1ccx = torchani.models.ANI1ccx().to(torch.float64).to(parser.device)
+ani1x = torchani.models.ANI1x().to(dtype).to(parser.device)
+ani1ccx = torchani.models.ANI1ccx().to(dtype).to(parser.device)
 
 
 def recursive_h5_files(base):
@@ -33,24 +35,21 @@ def recursive_h5_files(base):
             yield from recursive_h5_files(path)
 
 
-def by_batch(it):
-    for i in it:
-        # read
-        coordinates = torch.tensor(
-            i['coordinates'], dtype=torch.float64, device=parser.device,
-            requires_grad=True)
-        species = model.species_to_tensor(i['species']) \
-                       .unsqueeze(0).expand(coordinates.shape[0], -1)
-        energies = torch.tensor(i['energies'], dtype=torch.float64,
-                                device=parser.device)
-        forces = torch.tensor(i['forces'], dtype=torch.float64,
-                              device=parser.device)
-        yield from zip(
-            torch.split(coordinates, parser.batchsize),
-            torch.split(species, parser.batchsize),
-            torch.split(energies, parser.batchsize),
-            torch.split(forces, parser.batchsize),
-        )
+def by_batch(species, coordinates, model):
+    shape = species.shape
+    batchsize = max(1, parser.batchatoms // shape[1])
+    coordinates = coordinates.clone().detach().requires_grad_(True)
+    species = torch.split(species, batchsize)
+    coordinates = torch.split(coordinates, batchsize)
+    energies = []
+    forces = []
+    for s, c in tqdm.tqdm(zip(species, coordinates), total=len(species),
+                          position=1, desc="batch of {}x{}".format(*shape)):
+        _, e = model((s, c))
+        f, = torch.autograd.grad(e.sum(), c)
+        energies.append(e)
+        forces.append(f)
+    return torch.cat(energies).detach(), torch.cat(forces).detach()
 
 
 class Averager:
@@ -74,17 +73,25 @@ def relative_energies(energies):
 
 
 def do_benchmark(model):
-    dataset = by_batch(recursive_h5_files(parser.dir))
+    dataset = recursive_h5_files(parser.dir)
     mae_averager_energy = Averager()
     mae_averager_relative_energy = Averager()
     mae_averager_force = Averager()
     rmse_averager_energy = Averager()
     rmse_averager_relative_energy = Averager()
     rmse_averager_force = Averager()
-    for coordinates, species, energies, forces in tqdm.tqdm(dataset):
+    for i in tqdm.tqdm(dataset, position=0, desc="dataset"):
+        # read
+        coordinates = torch.tensor(
+            i['coordinates'], dtype=dtype, device=parser.device)
+        species = model.species_to_tensor(i['species']) \
+                       .unsqueeze(0).expand(coordinates.shape[0], -1)
+        energies = torch.tensor(i['energies'], dtype=dtype,
+                                device=parser.device)
+        forces = torch.tensor(i['forces'], dtype=dtype,
+                              device=parser.device)
         # compute
-        _, energies2 = model((species, coordinates))
-        forces2, = torch.autograd.grad(energies2.sum(), coordinates)
+        energies2, forces2 = by_batch(species, coordinates, model)
         ediff = energies - energies2
         relative_ediff = relative_energies(energies) - \
             relative_energies(energies2)
@@ -103,12 +110,12 @@ def do_benchmark(model):
         * HARTREE2KCAL
     mae_force = mae_averager_force.compute() * HARTREE2KCAL
     rmse_force = math.sqrt(rmse_averager_force.compute()) * HARTREE2KCAL
+    print(type(model).__name__)
     print("Energy:", mae_energy, rmse_energy)
     print("Relative Energy:", mae_relative_energy, rmse_relative_energy)
     print("Forces:", mae_force, rmse_force)
 
 
 for model in [ani1x, ani1ccx]:
-    print(type(model).__name__)
     do_benchmark(model)
     print()
