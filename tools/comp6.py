91eddc17c1be1be1bfc9f87be8e3a8d2eacf44da
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-03-22 15:02:12 -0400

    comp

--- tools/comp6.py
+++ tools/comp6.py
@@ -3,7 +3,10 @@ import torch
 import torchani
 from torchani.data._pyanitools import anidataloader
 import argparse
+import math
 
+
+HARTREE2KCAL = 627.509
 
 # parse command line arguments
 parser = argparse.ArgumentParser()
@@ -28,14 +31,66 @@ def recursive_h5_files(base):
             yield from recursive_h5_files(path)
 
 
+class Averager:
+
+    def __init__(self):
+        self.count = 0
+        self.cumsum = 0
+
+    def update(self, new):
+        assert len(new.shape) == 1
+        self.count += new.shape[0]
+        self.cumsum += new.sum().item()
+
+    def compute(self):
+        return self.cumsum / self.count
+
+
+def relative_energies(energies):
+    a, b = torch.combinations(energies, r=2).unbind(1)
+    return a - b
+
+
 def do_benchmark(model):
     dataset = recursive_h5_files(parser.dir)
+    mae_averager_energy = Averager()
+    mae_averager_relative_energy = Averager()
+    mae_averager_force = Averager()
+    rmse_averager_energy = Averager()
+    rmse_averager_relative_energy = Averager()
+    rmse_averager_force = Averager()
     for i in dataset:
-        coordinates = torch.tensor(i['coordinates'],
-            dtype=torch.float64, requires_grad=True)
+        # read
+        coordinates = torch.tensor(
+            i['coordinates'], dtype=torch.float64, requires_grad=True)
+        species = model.species_to_tensor(i['species']) \
+                       .unsqueeze(0).expand(coordinates.shape[0], -1)
         energies = torch.tensor(i['energies'], dtype=torch.float64)
-        species = model.species_to_tensor(i['species'])
         forces = torch.tensor(i['forces'], dtype=torch.float64)
+        # compute
+        _, energies2 = model((species, coordinates))
+        forces2, = torch.autograd.grad(energies2.sum(), coordinates)
+        ediff = energies - energies2
+        relative_ediff = relative_energies(energies) - \
+            relative_energies(energies2)
+        fdiff = forces.flatten() - forces2.flatten()
+        # update
+        mae_averager_energy.update(ediff.abs())
+        mae_averager_relative_energy.update(relative_ediff.abs())
+        mae_averager_force.update(fdiff.abs())
+        rmse_averager_energy.update(ediff ** 2)
+        rmse_averager_relative_energy.update(relative_ediff ** 2)
+        rmse_averager_force.update(fdiff ** 2)
+    mae_energy = mae_averager_energy.compute() * HARTREE2KCAL
+    rmse_energy = math.sqrt(rmse_averager_energy.compute()) * HARTREE2KCAL
+    mae_relative_energy = mae_averager_relative_energy.compute() * HARTREE2KCAL
+    rmse_relative_energy = math.sqrt(rmse_averager_relative_energy.compute()) \
+        * HARTREE2KCAL
+    mae_force = mae_averager_force.compute() * HARTREE2KCAL
+    rmse_force = math.sqrt(rmse_averager_force.compute()) * HARTREE2KCAL
+    print("Energy:", mae_energy, rmse_energy)
+    print("Relative Energy:", mae_relative_energy, rmse_relative_energy)
+    print("Forces:", mae_force, rmse_force)
 
 
 for model in [ani1x, ani1ccx]:
