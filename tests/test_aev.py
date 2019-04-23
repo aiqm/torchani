e150514bc3eb253653e967a20261d73257cdb8ce
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-04-23 16:08:04 -0400

    Add test

--- tests/test_aev.py
+++ tests/test_aev.py
@@ -54,6 +54,25 @@ class TestAEV(unittest.TestCase):
                 _, aev = self.aev_computer((species, coordinates))
                 self._assertAEVEqual(expected_radial, expected_angular, aev)
 
+    def testBenzeneMD(self):
+        for i in range(100):
+            datafile = os.path.join(path, 'test_data/benzene-md/{}.dat'.format(i))
+            with open(datafile, 'rb') as f:
+                coordinates, species, expected_radial, expected_angular, _, _, cell, pbc \
+                    = pickle.load(f)
+                coordinates = torch.from_numpy(coordinates).float().unsqueeze(0)
+                species = torch.from_numpy(species).unsqueeze(0)
+                expected_radial = torch.from_numpy(expected_radial).float().unsqueeze(0)
+                expected_angular = torch.from_numpy(expected_angular).float().unsqueeze(0)
+                cell = torch.from_numpy(cell).float()
+                pbc = torch.from_numpy(pbc)
+                coordinates = self.transform(coordinates)
+                species = self.transform(species)
+                expected_radial = self.transform(expected_radial)
+                expected_angular = self.transform(expected_angular)
+                _, aev = self.aev_computer((species, coordinates, cell, pbc))
+                self._assertAEVEqual(expected_radial, expected_angular, aev)
+
     def testPadding(self):
         species_coordinates = []
         radial_angular = []
