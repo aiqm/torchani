5ba720d503a2b599b64f438963d8802e6b677e9c
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2019-07-30 13:05:00 -0400

    fix

--- azure/install_dependencies_python2.sh
+++ azure/install_dependencies_python2.sh
@@ -3,4 +3,4 @@
 python -m pip install --upgrade pip
 pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 pip install tqdm pyyaml future
-pip2 install ase<=3.17.0￼
+pip2 install 'ase<=3.17'
