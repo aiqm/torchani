62f34e8d5c31235d3fad04f3edadb94f00abbfa6
Farhad Ramezanghorbani <farhadrgh@users.noreply.github.com>
2019-07-30 19:33:15 -0400

    ASE dropped python2 support (#265)

--- azure/install_dependencies_python2.sh
+++ azure/install_dependencies_python2.sh
@@ -3,4 +3,4 @@
 python -m pip install --upgrade pip
 pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 pip install tqdm pyyaml future
-pip install 'ase<=3.17'
+pip2 install 'ase<=3.17'
