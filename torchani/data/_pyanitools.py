d506b9f9df662ff388d3a13c25026b30a68bd4b0
ipickering <ign.pickering@gmail.com>
2019-08-01 17:30:08 -0400

    Make flake8 happy

--- torchani/data/_pyanitools.py
+++ torchani/data/_pyanitools.py
@@ -30,12 +30,10 @@ class datapacker:
 
 class anidataloader:
 
-
     def __init__(self, store_file):
         if not os.path.exists(store_file):
             exit('Error: file not found - ' + store_file)
         self.store = h5py.File(store_file, 'r')
-
 
     def h5py_dataset_iterator(self, g, prefix=''):
         """Group recursive iterator
@@ -57,30 +55,24 @@ class anidataloader:
                                 if isinstance(dataset[0], np.bytes_):
                                     dataset = [a.decode('ascii')
                                                for a in dataset]
-
                         data.update({k: dataset})
-
                 yield data
             else:  # test for group (go down)
                 yield from self.h5py_dataset_iterator(item, path)
-
 
     def __iter__(self):
         """Default class iterator (iterate through all data)"""
         for data in self.h5py_dataset_iterator(self.store):
             yield data
 
-
     def get_group_list(self):
         """Returns a list of all groups in the file"""
         return [g for g in self.store.values()]
-
 
     def iter_group(self, g):
         """Allows interation through the data in a given group"""
         for data in self.h5py_dataset_iterator(g):
             yield data
-
 
     def get_data(self, path, prefix=''):
         """Returns the requested dataset"""
@@ -100,7 +92,6 @@ class anidataloader:
                 data.update({k: dataset})
         return data
 
-
     def group_size(self):
         """Returns the number of groups"""
         return len(self.get_group_list())
@@ -110,7 +101,6 @@ class anidataloader:
         for g in self.store.values():
             count = count + len(g.items())
         return count
-
 
     def cleanup(self):
         """Close the HDF5 file"""
