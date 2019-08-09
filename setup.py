45c3b54c4eb6993cc33d07471db7c116bc59ba5d
Richard Xue <yueyericardo@gmail.com>
2019-08-09 18:44:48 -0400

    enable discription in pypi page (#285)
    https://pypi.org/project/torchani/

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 import sys
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup_attrs = {
     'name': 'torchani',
     'description': 'PyTorch implementation of ANI',
+    'long_description': long_description,
+    'long_description_content_type': "text/markdown",
     'url': 'https://github.com/zasdfgbnm/torchani',
     'author': 'Xiang Gao',
     'author_email': 'qasdfgtyuiop@ufl.edu',
