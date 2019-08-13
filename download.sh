faf1fc9c335233563e5a4d8c8b8875c6e9fd5c5d
Gao, Xiang <qasdfgtyuiop@gmail.com>
2019-08-13 16:27:56 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://www.dropbox.com/sh/2c8zdqc1hrqsgwy/AAD6l24ngoiFa6DRapF6HPk5a/ -O download.tar.xz
+wget https://www.dropbox.com/sh/2c8zdqc1hrqsgwy/AAD6l24ngoiFa6DRapF6HPk5a/ -O download.zip
 unzip download.zip -d download
