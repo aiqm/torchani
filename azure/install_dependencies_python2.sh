d0332ed6e59d4d3d5fe3303fa63aeacc4ded8f43
richard <yueyericardo@gmail.com>
2019-08-18 00:39:04 -0400

    add pkbar to test dependence

--- azure/install_dependencies_python2.sh
+++ azure/install_dependencies_python2.sh
@@ -3,4 +3,5 @@
 python -m pip install --upgrade pip
 pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 pip install tqdm pyyaml future
+pip install pkbar
 pip2 install 'ase<=3.17'
