1135bda9b29c1a155c8eef8794422409be181975
Gao, Xiang <qasdfgtyuiop@gmail.com>
2019-08-20 01:49:55 -0400

    Update install_dependencies_python2.sh

--- azure/install_dependencies_python2.sh
+++ azure/install_dependencies_python2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python -m pip install --upgrade pip
-pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+pip install torch -f https://download.pytorch.org/whl/nightly/cpu/torch.html
 pip install tqdm pyyaml future
 pip2 install 'ase<=3.17'
