c0f375648db642e83fa66c33a3445f9492d39454
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2019-09-05 11:48:15 -0400

    aev_computer dtype to double

--- torchani/ase.py
+++ torchani/ase.py
@@ -38,7 +38,8 @@ class Calculator(ase.calculators.calculator.Calculator):
         self.species_to_tensor = utils.ChemicalSymbolsToInts(species)
         # aev_computer.neighborlist will be changed later, so we need a copy to
         # make sure we do not change the original object
-        self.aev_computer = copy.deepcopy(aev_computer.to(dtype))
+        aev_computer = copy.deepcopy(aev_computer)
+        self.aev_computer = aev_computer.to(dtype)
         self.model = copy.deepcopy(model)
         self.energy_shifter = copy.deepcopy(energy_shifter)
         self.overwrite = overwrite
@@ -92,11 +93,11 @@ class Calculator(ase.calculators.calculator.Calculator):
                 strain_y = self.strain(cell, displacement_y, 1)
                 strain_z = self.strain(cell, displacement_z, 2)
                 cell = cell + strain_x + strain_y + strain_z
-            _, aevs = self.aev_computer((species, coordinates), cell=cell, pbc=pbc)
+            _, aev = self.aev_computer((species, coordinates), cell=cell, pbc=pbc)
         else:
-            _, aevs = self.aev_computer((species, coordinates))
+            _, aev = self.aev_computer((species, coordinates))
 
-        _, energy = self.nn((species, aevs))
+        _, energy = self.nn((species, aev))
         energy *= ase.units.Hartree
         self.results['energy'] = energy.item()
         self.results['free_energy'] = energy.item()
