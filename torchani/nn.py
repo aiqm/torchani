47033682568ce7476eb55364ad0530819b7ad8c8
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-10-09 10:08:40 -0700

    more

--- torchani/nn.py
+++ torchani/nn.py
@@ -1,5 +1,4 @@
 import torch
-from . import utils
 from typing import Tuple
 
 
@@ -52,39 +51,43 @@ class ANIModel(torch.nn.Module):
 class Ensemble(torch.nn.Module):
     """Compute the average output of an ensemble of modules."""
 
-    def __init__(self, modules):
-        super(Ensemble, self).__init__()
-        self.modules_list = torch.nn.ModuleList(modules)
-
-    def __getitem__(self, i):
-        return self.modules_list[i]
-
-    def forward(self, species_input):
-        # type: (Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]
-        outputs = [x(species_input)[1] for x in self.modules_list]
-        species, _ = species_input
-        return species, sum(outputs) / len(outputs)
+    # FIXME: due to PyTorch bug, we have to hard code the
+    # ensemble size to 8.
 
     # def __init__(self, modules):
     #     super(Ensemble, self).__init__()
-    #     assert len(modules) == 8
-    #     self.model0 = modules[0]
-    #     self.model1 = modules[1]
-    #     self.model2 = modules[2]
-    #     self.model3 = modules[3]
-    #     self.model4 = modules[4]
-    #     self.model5 = modules[5]
-    #     self.model6 = modules[6]
-    #     self.model7 = modules[7]
+    #     self.modules_list = torch.nn.ModuleList(modules)
 
     # def forward(self, species_input):
     #     # type: (Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]
+    #     outputs = [x(species_input)[1] for x in self.modules_list]
     #     species, _ = species_input
-    #     sum_ = self.model0(species_input) + self.model1(species_input) \
-    #          + self.model2(species_input) + self.model3(species_input) \
-    #          + self.model4(species_input) + self.model5(species_input) \
-    #          + self.model6(species_input) + self.model7(species_input)
-    #     return species, sum_ / 8.0
+    #     return species, sum(outputs) / len(outputs)
+
+    def __init__(self, modules):
+        super(Ensemble, self).__init__()
+        assert len(modules) == 8
+        self.model0 = modules[0]
+        self.model1 = modules[1]
+        self.model2 = modules[2]
+        self.model3 = modules[3]
+        self.model4 = modules[4]
+        self.model5 = modules[5]
+        self.model6 = modules[6]
+        self.model7 = modules[7]
+
+    def __getitem__(self, i):
+        return [self.model0, self.model1, self.model2, self.model3,
+                self.model4, self.model5, self.model6, self.model7][i]
+
+    def forward(self, species_input):
+        # type: (Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]
+        species, _ = species_input
+        sum_ = self.model0(species_input)[1] + self.model1(species_input)[1] \
+            + self.model2(species_input)[1] + self.model3(species_input)[1] \
+            + self.model4(species_input)[1] + self.model5(species_input)[1] \
+            + self.model6(species_input)[1] + self.model7(species_input)[1]
+        return species, sum_ / 8.0
 
 
 class Sequential(torch.nn.Module):
