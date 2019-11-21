53fe9e492a2069b524b6f2fbe9da9885d425c316
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-11-20 16:22:57 -0800

    flake8

--- tests/test_periodic_table_indexing.py
+++ tests/test_periodic_table_indexing.py
@@ -35,11 +35,11 @@ class TestBuiltinNetPeriodicTableIndex(unittest.TestCase):
         model1 = torchani.models.ANI1x()
         model2 = torchani.models.ANI1x(periodic_table_index=True)
         coordinates = torch.tensor([[[0.03192167, 0.00638559, 0.01301679],
-                             [-0.83140486, 0.39370209, -0.26395324],
-                             [-0.66518241, -0.84461308, 0.20759389],
-                             [0.45554739, 0.54289633, 0.81170881],
-                             [0.66091919, -0.16799635, -0.91037834]]],
-                           requires_grad=True)
+                                     [-0.83140486, 0.39370209, -0.26395324],
+                                     [-0.66518241, -0.84461308, 0.20759389],
+                                     [0.45554739, 0.54289633, 0.81170881],
+                                     [0.66091919, -0.16799635, -0.91037834]]],
+                                   requires_grad=True)
         species1 = model1.species_to_tensor('CHHHH').unsqueeze(0)
         species2 = torch.tensor([[6, 1, 1, 1, 1]])
 
