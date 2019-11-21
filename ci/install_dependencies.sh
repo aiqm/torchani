939ab88704e9bc908def86898ffc9b583af28507
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-11-20 17:45:54 -0800

    more

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -2,4 +2,4 @@
 
 python -m pip install --upgrade pip
 pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
-pip install tqdm pyyaml future pkbar ase
+pip install tqdm pyyaml pkbar ase
