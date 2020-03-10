b2080971aa326501ab2e158625a4516e6a5a54db
ipickering <ign.pickering@gmail.com>
2020-03-10 16:44:01 -0400

    Add ASE's hartree_to_ev conversion to compare with codata 2018

--- torchani/units.py
+++ torchani/units.py
@@ -17,6 +17,7 @@ https://doi.org/10.1351/goldbook.
 
 where the thermochemical calorie was used
 """
+import math
 
 HARTREE_TO_EV = 2.7211386245988e1 # uncertainty (53)
 HARTREE_TO_JOULE = 4.3597447222071e-18 # uncertainty (85)
@@ -32,6 +33,22 @@ HARTREE_TO_JOULEMOL = HARTREE_TO_JOULE * AVOGADROS_NUMBER
 EV_TO_KCALMOL = EV_TO_JOULE * JOULE_TO_KCAL * AVOGADROS_NUMBER
 EV_TO_JOULEMOL = EV_TO_JOULE * AVOGADROS_NUMBER
 
+# ASE uses its own definition of HARTREE_TO_EV, which they take from CODATA 2014
+# as of 03/10/2019, here we make sure that our definitions match to a given accuracy
+try:
+    from ase.units import Hartree as ASE_HARTREE_TO_EV
+    if not math.isclose(ASE_HARTREE_TO_EV, HARTREE_TO_EV, rel_tol=1e-8):
+        # raise an exception if for some reason ANI ans ASE differ in their 
+        # conversion factors, this should never happen
+        raise RuntimeError('ASE and ANI differ in their ha2ev conversions')
+
+    def ase_hartree2ev(x):
+        """Hartree to Electronvolt conversion factor
+        taken directly from ASE (as of 03/10/2019 it is the 2014 CODATA value """
+        return x * ASE_HARTREE_TO_EV
+except ImportError:
+    ASE_HARTREE_TO_EV = None
+
 
 def hartree2ev(x):
     """Hartree to Electronvolt conversion factor
@@ -44,6 +61,7 @@ def ev2joulemol(x):
     taken directly from 2018 CODATA recommended values"""
     return x * EV_TO_JOULEMOL
 
+
 def ev2kcalmol(x):
     """Electronvolt to (thermochemical) kilocalorie/mole conversion factor, derived
     from the Electronvolt-to-Joule conversion factor, Avogadro's number (both CODATA)
@@ -51,10 +69,12 @@ def ev2kcalmol(x):
     conversion factor (IUPAC)"""
     return x * EV_TO_KCALMOL
 
+
 def hartree2joulemol(x):
     """Hartree to Joule/mole conversion factor
     taken directly from 2018 CODATA recommended values"""
     return x * HARTREE_TO_JOULEMOL
+
 
 def hartree2kcalmol(x):
     """Hartree to (thermochemical) kilocalorie/mole conversion factor, derived
@@ -62,4 +82,3 @@ def hartree2kcalmol(x):
     and the Joule-to-kcal
     conversion factor (IUPAC)"""
     return x * HARTREE_TO_KCALMOL
-
