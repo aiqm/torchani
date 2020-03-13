d65116995d54ad8105cd7256c88ebf04bb7f7f1b
Ignacio Pickering <ign.pickering@gmail.com>
2020-03-13 13:16:41 -0400

    ChemicalSymbolsToInts is being used wrong (#425)
    * Add warning to ChemicalSymbolsToInts
    
    * Change ocurrences of ChemicalSymbolsToInts in the code to reflect safe usage
    
    * flake8
    
    * Add warning to call also
    
    * Add clarification
    
    * docs dont fetch magic functions so documentation is moved to the class docstring
    
    * fix clarification
    
    * flake8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ import torchani
 class TestUtils(unittest.TestCase):
 
     def testChemicalSymbolsToInts(self):
-        str2i = torchani.utils.ChemicalSymbolsToInts('ABCDEF')
+        str2i = torchani.utils.ChemicalSymbolsToInts(['A', 'B', 'C', 'D', 'E', 'F'])
         self.assertEqual(len(str2i), 6)
         self.assertListEqual(str2i('BACCC').tolist(), [1, 0, 2, 2, 2])
 
