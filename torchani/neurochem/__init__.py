14b9a395bdc4ab1cf09f45f1aeb0c8505d0338fa
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2020-05-11 15:12:01 +0200

    Change AdamW from TorchANI to PyTorch (#464)
    * switch AdamW from torchany to pytorch
    
    * fix __init__
    
    Co-authored-by: Farhad Ramezanghorbani <farhadrgh@users.noreply.github.com>

--- torchani/neurochem/__init__.py
+++ torchani/neurochem/__init__.py
@@ -14,7 +14,7 @@ import sys
 from ..nn import ANIModel, Ensemble, Gaussian, Sequential
 from ..utils import EnergyShifter, ChemicalSymbolsToInts
 from ..aev import AEVComputer
-from ..optim import AdamW
+from torch.optim import AdamW
 from collections import OrderedDict
 from torchani.units import hartree2kcalmol
 
