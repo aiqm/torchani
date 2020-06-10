4edbf05a39d4a1917e148dafbb9da94c72d74cd9
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2020-06-09 23:34:28 -0400

    rm svn checkout for resources

--- download.sh
+++ download.sh
@@ -3,6 +3,3 @@
 # download the data
 wget https://www.dropbox.com/sh/2c8zdqc1hrqsgwy/AAD6l24ngoiFa6DRapF6HPk5a/ -O download.zip
 unzip download.zip -d download || [[ $? == 2 ]]  # unzip return 2 for dropbox created zip file
-
-# downlaod the built-in model parameters from https://github.com/isayev/ASE_ANI
-svn checkout https://github.com/isayev/ASE_ANI/trunk/ani_models ./torchani/resources
