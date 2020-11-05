071df777dad5ec7b7e9ddb49c0de8b472671839a
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-04 20:58:22 -0800

    refactor ci

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -4,3 +4,4 @@ pip install --upgrade pip
 pip install twine wheel
 pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
 pip install -r test_requirements.txt
+pip install -r docs_requirements.txt
