1c0ad56fbc00c291d264e7699b791174a1570b02
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-04 19:50:07 -0800

    save

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -1,2 +1,8 @@
 CUDA_VERSION=$1
 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
