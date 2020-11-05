818c73aef3e99c2b15682b83e7067326e3e92475
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-04 19:52:12 -0800

    no vision

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install --upgrade pip
-pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
+pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
 pip install -r test_requirements.txt
