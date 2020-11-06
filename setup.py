c3e44c03ba2d03265af1d63fbabfb7ba0508763e
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-05 16:03:50 -0800

    Search for cub in pytorch include paths

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
 import os
 import subprocess
 from setuptools import setup, find_packages
+from distutils import log
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+def maybe_download_cub():
+    import torch
+    dirs = torch.utils.cpp_extension.include_paths(cuda=True)
+    for d in dirs:
+        cubdir = os.path.join(d, 'cub')
+        log.info(f'Searching for cub at {cubdir}...')
+        if os.path.isdir(cubdir):
+            log.info(f'Found cub in {cubdir}')
+            return []
+    # if no cub, download it to include dir from github
+    if not os.path.isdir('./include/cub'):
+        if not os.path.exists('./include'):
+            os.makedirs('include')
+        commands = """
+        echo "Downloading CUB library";
+        wget -q https://github.com/NVIDIA/cub/archive/main.zip;
+        unzip -q main.zip -d include;
+        mv include/cub-main/cub include;
+        echo "Removing unnecessary files";
+        rm main.zip;
+        rm -rf include/cub-main;
+        """
+        subprocess.run(commands, shell=True, check=True, universal_newlines=True)
+    return [os.path.abspath("./include")]
 
 
 def cuda_extension():
@@ -13,7 +40,6 @@ def cuda_extension():
     nvcc_args = ["-gencode=arch=compute_50,code=sm_50", "-gencode=arch=compute_60,code=sm_60",
                  "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
                  "-Xptxas=-v", '--expt-extended-lambda', '-use_fast_math']
-    include_dirs = []
     cuda_version = float(torch.version.cuda)
     if cuda_version >= 10:
         nvcc_args.append("-gencode=arch=compute_75,code=sm_75")
@@ -21,27 +47,11 @@ def cuda_extension():
         nvcc_args.append("-gencode=arch=compute_80,code=sm_80")
     if cuda_version >= 11.1:
         nvcc_args.append("-gencode=arch=compute_86,code=sm_86")
-    if cuda_version < 11:
-        # if no cub, download it to include dir from github
-        if not os.path.isdir('./include/cub'):
-            if not os.path.exists('./include'):
-                os.makedirs('include')
-            commands = """
-            echo "Downloading CUB library";
-            wget -q https://github.com/NVIDIA/cub/archive/main.zip;
-            unzip -q main.zip -d include;
-            mv include/cub-main/cub include;
-            echo "Removing unnecessary files";
-            rm main.zip;
-            rm -rf include/cub-main;
-            """
-            subprocess.run(commands, shell=True, check=True, universal_newlines=True)
-        include_dirs.append(os.path.abspath("./include"))
     return CUDAExtension(
         name='_real_cuaev',
         pkg='torchani.cuaev._real_cuaev',
         sources=['torchani/cuaev/aev.cu'],
-        include_dirs=include_dirs,
+        include_dirs=maybe_download_cub(),
         extra_compile_args={'cxx': ['-std=c++14'],
                             'nvcc': nvcc_args},
         optional=True)
@@ -59,6 +69,8 @@ def cuda_extension_kwargs():
             })
         return cuda_extension_kwargs
     except OSError:
+        return {}
+    except ImportError:
         return {}
 
 
