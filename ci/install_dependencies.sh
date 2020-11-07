821227fcc294d20b69b8c0393369a9037e180af5
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-06 23:03:13 -0800

    Make CUDA extension TorchScript compatible

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -2,6 +2,9 @@
 
 pip install --upgrade pip
 pip install twine wheel
-pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
+# pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
+wget --verbose https://www.dropbox.com/s/f6pf5jzbhut3yu1/torch-1.8.0a0-cp38-cp38-linux_x86_64.whl?dl=0 -o torch.whl
+pip install torch.whl
+
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
