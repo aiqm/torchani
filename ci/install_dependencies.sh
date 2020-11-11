a06db6bd09a56ba6c8da40f3348a302851633d66
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-10 23:26:00 -0800

    save

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -3,6 +3,5 @@
 pip install --upgrade pip
 pip install twine wheel
 pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
-
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
