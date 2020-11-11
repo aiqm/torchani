e587e4c51645420e3200baee844636bd40e686cf
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-11 00:05:31 -0800

    revert

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -46,7 +46,6 @@ has_cuaev = 'torchani.cuaev' in importlib_metadata.metadata(__package__).get_all
 if has_cuaev:
     # We need to import torchani.cuaev to tell PyTorch to initialize torch.ops.cuaev
     import cuaev  # noqa: F401
-    cuaev = torch.ops.cuaev  # noqa: F811
 else:
     warnings.warn("cuaev not installed")
 
