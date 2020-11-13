abb9b008deed3e6202e1686fb3b6542f3924f47b
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-11-12 19:55:50 -0800

    Revert "Use PyTorch autograd's hessian (#532)"
    This reverts commit bd9d888a1ba92e2af7a4689880c2a42528618903.

--- torchani/utils.py
+++ torchani/utils.py
@@ -241,6 +241,43 @@ class ChemicalSymbolsToInts:
         return len(self.rev_species)
 
 
+def _get_derivatives_not_none(x: Tensor, y: Tensor, retain_graph: Optional[bool] = None, create_graph: bool = False) -> Tensor:
+    ret = torch.autograd.grad([y.sum()], [x], retain_graph=retain_graph, create_graph=create_graph)[0]
+    assert ret is not None
+    return ret
+
+
+def hessian(coordinates: Tensor, energies: Optional[Tensor] = None, forces: Optional[Tensor] = None) -> Tensor:
+    """Compute analytical hessian from the energy graph or force graph.
+
+    Arguments:
+        coordinates (:class:`torch.Tensor`): Tensor of shape `(molecules, atoms, 3)`
+        energies (:class:`torch.Tensor`): Tensor of shape `(molecules,)`, if specified,
+            then `forces` must be `None`. This energies must be computed from
+            `coordinates` in a graph.
+        forces (:class:`torch.Tensor`): Tensor of shape `(molecules, atoms, 3)`, if specified,
+            then `energies` must be `None`. This forces must be computed from
+            `coordinates` in a graph.
+
+    Returns:
+        :class:`torch.Tensor`: Tensor of shape `(molecules, 3A, 3A)` where A is the number of
+        atoms in each molecule
+    """
+    if energies is None and forces is None:
+        raise ValueError('Energies or forces must be specified')
+    if energies is not None and forces is not None:
+        raise ValueError('Energies or forces can not be specified at the same time')
+    if forces is None:
+        assert energies is not None
+        forces = -_get_derivatives_not_none(coordinates, energies, create_graph=True)
+    flattened_force = forces.flatten(start_dim=1)
+    force_components = flattened_force.unbind(dim=1)
+    return -torch.stack([
+        _get_derivatives_not_none(coordinates, f, retain_graph=True).flatten(start_dim=1)
+        for f in force_components
+    ], dim=1)
+
+
 class FreqsModes(NamedTuple):
     freqs: Tensor
     modes: Tensor
@@ -280,8 +317,6 @@ def vibrational_analysis(masses, hessian, mode_type='MDU', unit='cm^-1'):
         raise ValueError('Only meV and cm^-1 are supported right now')
 
     assert hessian.shape[0] == 1, 'Currently only supporting computing one molecule a time'
-    degree_of_freedom = hessian.shape[1] * hessian.shape[2]
-    hessian = hessian.reshape(1, degree_of_freedom, degree_of_freedom)
     # Solving the eigenvalue problem: Hq = w^2 * T q
     # where H is the Hessian matrix, q is the normal coordinates,
     # T = diag(m1, m1, m1, m2, m2, m2, ....) is the mass
@@ -388,5 +423,6 @@ PERIODIC_TABLE = """
     """.strip().split()
 
 
-__all__ = ['pad_atomic_properties', 'present_species', 'vibrational_analysis',
-           'strip_redundant_padding', 'ChemicalSymbolsToInts', 'get_atomic_masses']
+__all__ = ['pad_atomic_properties', 'present_species', 'hessian',
+           'vibrational_analysis', 'strip_redundant_padding',
+           'ChemicalSymbolsToInts', 'get_atomic_masses']
