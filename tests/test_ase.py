56474cb9e2bfbe286d93ca9cd3a230c758f47bb5
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-11-12 19:20:38 -0800

    save

--- tests/test_ase.py
+++ tests/test_ase.py
@@ -8,6 +8,7 @@ import torch
 import torchani
 import unittest
 import os
+from torch.testing._internal.common_utils import TestCase
 
 path = os.path.dirname(os.path.realpath(__file__))
 tol = 5e-5
@@ -21,7 +22,7 @@ def get_numeric_force(atoms, eps):
     return fn
 
 
-class TestASE(unittest.TestCase):
+class TestASE(TestCase):
 
     def setUp(self):
         self.model = torchani.models.ANI1x(model_index=0).double()
@@ -32,12 +33,9 @@ class TestASE(unittest.TestCase):
         atoms.set_calculator(calculator)
         dyn = Langevin(atoms, 5 * units.fs, 30000000 * units.kB, 0.002)
         dyn.run(100)
-        f = torch.from_numpy(atoms.get_forces())
+        f = atoms.get_forces()
         fn = get_numeric_force(atoms, 0.001)
-        df = (f - fn).abs().max()
-        avgf = f.abs().mean()
-        if avgf > 0:
-            self.assertLess(df / avgf, 0.1)
+        self.assertEqual(f, fn, rtol=0.1, atol=0)
 
     def testWithNumericalStressWithPBCEnabled(self):
         filename = os.path.join(path, '../tools/generate-unit-test-expect/others/Benzene.cif')
@@ -52,8 +50,7 @@ class TestASE(unittest.TestCase):
         def test_stress():
             stress = benzene.get_stress()
             numerical_stress = calculator.calculate_numerical_stress(benzene)
-            diff = torch.from_numpy(stress - numerical_stress).abs().max().item()
-            self.assertLess(diff, tol)
+            self.assertEqual(stress, numerical_stress)
         dyn.attach(test_stress, interval=30)
         dyn.run(120)
 
