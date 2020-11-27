7710e029d6eeccec1ac3d9be735bbbbb2e3f2728
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-11-27 03:16:47 -0800

    Update extension-include-dirs.py

--- ci/extension-include-dirs.py
+++ ci/extension-include-dirs.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils import cpp_extension
 from sysconfig import get_paths
 
-pt = torch.utils.cpp_extension.include_paths(cuda=True)
+pt = cpp_extension.include_paths(cuda=True)
 py = get_paths()
 dirs = ('', *pt, py['include'])
 print(' -I'.join(dirs))
