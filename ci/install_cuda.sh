67110d8efc4525631d23783331d452503bfab4fc
Jinze Xue <yueyericardo@gmail.com>
2021-02-27 13:53:15 -0500

    Update install_cuda.sh

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -7,4 +7,4 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-command-line-tools-11-0 cuda-libraries-dev-11-0
