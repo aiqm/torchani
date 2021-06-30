b5a01f0ffd893466dae43a4f2d30e9691f32e7e8
Jinze (Richard) Xue <yueyericardo@gmail.com>
2021-06-30 12:48:12 -0400

    Stable pytorch 1.9.0 (#71)
    * Update install_stable_dependencies.sh
    
    * workflow name
    
    * save
    
    * fix
    
    * use conda for stable test
    
    * save
    
    * save
    
    * fix
    
    * save
    
    * fix
    
    * save
    
    * fix
    
    * save

--- ci/install_stable_dependencies.sh
+++ ci/install_stable_dependencies.sh
@@ -2,6 +2,6 @@
 
 pip install --upgrade pip
 pip install twine wheel
-pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html --upgrade
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html --upgrade
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
