4c80fcc3ff79e1a9f4465f9a0f43626eeccedef6
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-03-24 17:23:29 +0000

    use torch.div instead of //

--- torchani/aev.py
+++ torchani/aev.py
@@ -246,7 +246,7 @@ def triple_by_molecule(atom_index12: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
     uniqued_central_atom_index, counts = torch.unique_consecutive(sorted_ai1, return_inverse=False, return_counts=True)
 
     # compute central_atom_index
-    pair_sizes = counts * (counts - 1) // 2
+    pair_sizes = torch.div(counts * (counts - 1), 2, rounding_mode='trunc')
     pair_indices = torch.repeat_interleave(pair_sizes)
     central_atom_index = uniqued_central_atom_index.index_select(0, pair_indices)
 
