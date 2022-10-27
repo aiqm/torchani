Fix smooth cutoff function hessian INFs/NaNs (#180)

examples/modular_aev_example.py
@@ -108,7 +108,7 @@ class AngularCosDiff(torch.nn.Module):
         term1 = self.Gamma * (cos_angles - torch.cos(self.ShfZ))**2
         term2 = self.EtaA * (distances12.sum(0) / 2 - self.ShfA)**2
         exponent = term1 + term2
-        ret = 4 * torch.exp(-exponent) * fcj12.prod(0)
+        ret = 4 * torch.exp(-exponent) * (fcj12[0] * fcj12[1])
         out = ret.flatten(start_dim=1)
         return out
 

torchani/aev/aev_terms.py
@@ -152,7 +152,10 @@ class StandardAngular(torch.nn.Module):
         fcj12 = self.cutoff_fn(distances12, self.cutoff)
         factor1 = ((1 + torch.cos(angles - self.ShfZ)) / 2)**self.Zeta
         factor2 = torch.exp(-self.EtaA * (distances12.sum(0) / 2 - self.ShfA)**2)
-        ret = 2 * factor1 * factor2 * fcj12.prod(0)
+        # Use `fcj12[0] * fcj12[1]` instead of `fcj12.prod(0)` to avoid the INFs/NaNs
+        # problem for smooth cutoff function, for more detail please check issue:
+        # https://github.com/roitberg-group/torchani_sandbox/issues/178
+        ret = 2 * factor1 * factor2 * (fcj12[0] * fcj12[1])
         # At this point, ret now has shape
         # (conformations x atoms, ?, ?, ?, ?) where ? depend on constants.
         # We then should flat the last 4 dimensions to view the subAEV as a two