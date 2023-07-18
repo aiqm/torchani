c82b349e2d5620c228eeef67e3a2893b801104e9
Ignacio Pickering <ign.pickering@gmail.com>
2023-07-17 22:22:59 -0400

    Add Yinuo's Diels Alder reaction data and CC-BondScan data (#218)
    * Simplify input to functions
    
    * fix flake8
    
    * fix bug
    
    * fix bug
    
    * Partial fixes for bugs
    
    * fix wrapper
    
    * Fix bugs
    
    * Make static method into function
    
    * move cutoffs and neighbors outside aev
    
    * Organize imports
    
    * Organize imports
    
    * Fix imports in tests
    
    * Fix imports
    
    * Organize imports in tools
    
    * Remove coding directives (unneeded in py3)
    
    * Simplify repulsion example
    
    * Remove repulsion from api, use potentials instead
    
    * Cleanup builtin datasets code
    
    * Fix test
    
    * Cleanup builtin datasets
    
    * Fix typo between 1x and 2x
    
    * Fix import
    
    * Fix formatting
    
    * Add ani ccscan data
    
    * Add documentation
    
    * Fix md5s file
    
    * Fix default lot
    
    * Add diels alder data
    
    * Add option to automatically update files inside repo
    
    * Improve cli utility
    
    * fix bug with update
    
    * Add CLI utility to pack datasets into a format that ani understands
    
    * Add possibility of modifying main part name for files
    
    * Newline at end of file

--- torchani/datasets/builtin.py
+++ torchani/datasets/builtin.py
@@ -74,6 +74,19 @@ wavefunction_method/basis_set when appropriate.
   (disjoint from LightIons and IonsHeavy)
   This dataset is not meant to be trained to on its own.
 
+- ANI-CCScan, with LoT:
+    - B973c/def2mTZVP
+    - UwB97x/6-31Gd
+  Dataset includes relaxed scans of stretching carbon-carbon single bonds
+  for a number of compounds. UwB97x means "Unrestricted" Kohn-Sham was used.
+
+- DielsAlder, with LoT:
+    - UwB97x/6-31Gd
+  Dataset includes structures sampled in different ways from a diels alder
+  reaction. Among them, from the intrinsic reaction coordinate (IRC), and from
+  specific points of the potential energy surface (PES), in some cases using
+  active learning, with the QBC criteria, and with normal mode sampling.
+  UwB97x means "Unrestricted" Kohn-Sham was used.
 - TestData, with LoT:
     - wB97X/631Gd
   GDB subset, only for debugging and code testing purposes.
