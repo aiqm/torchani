429951ddf7221118d26a1d3f834280abe34ea3dc
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2024-03-13 12:52:56 +0000

    removed parts we dont need

--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# download the data
-wget https://www.dropbox.com/sh/2c8zdqc1hrqsgwy/AAD6l24ngoiFa6DRapF6HPk5a/ -O download.zip
-unzip download.zip -d download || [[ $? == 2 ]]  # unzip return 2 for dropbox created zip file
--- tools/generate-unit-test-expect/nist-dataset/README.md
+++
@@ -1,7 +0,0 @@
-# Commands
-
-## Download all 3d structures from NIST
-
-```
-scrapy runspider nist.py -o result.json
-```
