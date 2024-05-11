93db0d802369d5072ad9546b12d70bcc2e37ced1
Ignacio Pickering <ign.pickering@gmail.com>
2024-05-11 01:20:39 -0400

    Maintain/ipickering/run gradtest with jit and cuda (#308)
    * Merge padding with utils test
    
    * Run gradtest with jit and cuda

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -1,61 +1,61 @@
-import torch
-import torchani
 import unittest
 import os
 import pickle
-from torchani.testing import TestCase
+
+import torch
+
+import torchani
+from torchani.testing import ANITest, expand
 
 path = os.path.dirname(os.path.realpath(__file__))
 
 
-class TestGrad(TestCase):
-    # torch.autograd.gradcheck and torch.autograd.gradgradcheck verify that
-    # the numerical and analytical gradient and hessian of a function
-    # matches within a given tolerance.
+@expand()
+class TestGrad(ANITest):
+    # torch.autograd.gradcheck (and torch.autograd.gradgradcheck) verify that
+    # the numerical and analytical gradient (and hessian respectively) of a
+    # function match within a given tolerance.
     #
     # The forward call of the function is wrapped with a lambda so that
     # gradcheck gets a function with only one tensor input and tensor output.
-
+    #
     # nondet_tol is necessarily greater than zero since some operations are
     # nondeterministic which makes two equal inputs have different outputs
-
     def setUp(self):
-        self.device = torch.device(
-            'cuda' if torch.cuda.is_available() else 'cpu')
-
-        self.model = torchani.models.ANI1x(model_index=0, periodic_table_index=False).to(device=self.device,
-                                                             dtype=torch.double)
-        datafile = os.path.join(path, 'test_data/NIST/all')
-
+        self.model = self._setup(
+            torchani.models.ANI1x(model_index=0, periodic_table_index=False).double()
+        )
+        datafile = os.path.join(path, "test_data/NIST/all")
         # Some small molecules are selected to make the tests faster
-        self.data = pickle.load(open(datafile, 'rb'))[1243:1250]
+        with open(datafile, mode="rb") as fb:
+            data = pickle.load(fb)[1243:1250]
+        self.data = data
 
     def testGradCheck(self):
         for coordinates, species, _, _, _, _ in self.data:
+            coordinates = torch.tensor(
+                coordinates, device=self.device, dtype=torch.double, requires_grad=True
+            )
+            species = torch.tensor(species, device=self.device, dtype=torch.long)
 
-            coordinates = torch.from_numpy(coordinates).to(device=self.device,
-                                                           dtype=torch.float64)
-            coordinates.requires_grad_(True)
-
-            species = torch.from_numpy(species).to(self.device)
-
-            torch.autograd.gradcheck(lambda x: self.model((species, x)).energies,
-                                     coordinates,
-                                     nondet_tol=1e-13)
+            torch.autograd.gradcheck(
+                lambda x: self.model((species, x)).energies,
+                coordinates,
+                nondet_tol=1e-13,
+            )
 
     def testGradGradCheck(self):
         for coordinates, species, _, _, _, _ in self.data:
+            coordinates = torch.tensor(
+                coordinates, device=self.device, dtype=torch.double, requires_grad=True
+            )
+            species = torch.tensor(species, device=self.device, dtype=torch.long)
+            torch.autograd.gradgradcheck(
+                lambda x: self.model((species, x)).energies,
+                coordinates,
+                nondet_tol=1e-13,
+            )
 
-            coordinates = torch.from_numpy(coordinates).to(device=self.device,
-                                                           dtype=torch.float64)
-            coordinates.requires_grad_(True)
 
-            species = torch.from_numpy(species).to(self.device)
-
-            torch.autograd.gradgradcheck(lambda x: self.model((species, x)).energies,
-                                         coordinates,
-                                         nondet_tol=1e-13)
-
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
