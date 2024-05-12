063589d62c0ab351ba7405535ea1a0ebdc7eb709
Ignacio Pickering <ign.pickering@gmail.com>
2024-05-12 18:10:47 -0400

    Upgrade calc test (#322)

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,34 +1,41 @@
 import unittest
 import torch
-import torchani
-from torchani.testing import TestCase
+from torchani.calc import compute_dipole
+from torchani.testing import ANITest, expand
 
 
-class TestCalc(TestCase):
-    def testComputeDiploes(self):
-        species = torch.tensor([[1, 1, 8]], dtype=torch.long)
-        coordinates = torch.tensor(
-            [[[-1, 0, 0], [1, 0, 0], [0, 1, 0]]], dtype=torch.float
+@expand(jit=False)
+class TestDipoles(ANITest):
+    def setUp(self) -> None:
+        self.species = torch.tensor([[1, 1, 8]], dtype=torch.long, device=self.device)
+        self.coordinates = torch.tensor(
+            [[[-1, 0, 0], [1, 0, 0], [0, 1, 0]]], dtype=torch.float, device=self.device
         )
-        charges = torch.tensor([[0.1, 0.1, -0.2]])
 
-        dipoles = torchani.calc.compute_dipole(
+    def testZeroNetCharge(self):
+        species = self.species
+        coordinates = self.coordinates
+        charges = torch.tensor([[0.1, 0.1, -0.2]], device=self.device)
+        dipoles = compute_dipole(
             species, coordinates, charges, center_of_mass=True
         )
         self.assertEqual(dipoles, [[0.0000, -0.2000, 0.0000]])
 
-        dipoles = torchani.calc.compute_dipole(
+        dipoles = compute_dipole(
             species, coordinates, charges, center_of_mass=False
         )
         self.assertEqual(dipoles, [[0.0000, -0.2000, 0.0000]])
 
-        charges = torch.tensor([[0.1, 0.1, -0.4]])
-        dipoles = torchani.calc.compute_dipole(
+    def testNonZeroNetCharge(self):
+        species = self.species
+        coordinates = self.coordinates
+        charges = torch.tensor([[0.1, 0.1, -0.4]], device=self.device)
+        dipoles = compute_dipole(
             species, coordinates, charges, center_of_mass=True
         )
         self.assertEqual(dipoles, [[0.0000, -0.2223813533782959, 0.0000]])
 
-        dipoles = torchani.calc.compute_dipole(
+        dipoles = compute_dipole(
             species, coordinates, charges, center_of_mass=False
         )
         self.assertEqual(dipoles, [[0.0000, -0.4000, 0.0000]])
