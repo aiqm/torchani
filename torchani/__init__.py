81322a260e31360cc82437e787e3df43596e22b1
Ignacio Pickering <ign.pickering@gmail.com>
2024-05-22 15:11:11 -0400

    Some formatting and comments (#354)

--- torchani/__init__.py
+++ torchani/__init__.py
@@ -53,13 +53,12 @@ from torchani import (
     constants,
     data,  # TODO: Get rid of this
 )
-# NOTE: ase is an optional dependency so we don't import it here
+# NOTE: ase and neurochem are optional dependencies so we don't import those here
 
 try:
     __version__ = version("torchani")
 except PackageNotFoundError:
-    # package is not installed
-    pass
+    pass  # package is not installed
 
 __all__ = [
     'AEVComputer',
@@ -102,7 +101,7 @@ if torch.cuda.is_available():
             " To suppress warning set the env var TORCHANI_NO_WARN_TF32 to any value"
         )
 
-# Optional dependencies
+# Optional submodules, depend on 'ase' and 'lark-parser' being available
 try:
     from . import ase  # noqa: F401
     __all__.append('ase')
