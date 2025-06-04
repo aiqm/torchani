Fix hardcoded float32 precision (#578)
Fix hardcoded F32 precision in some cases

torchani/electro.py
@@ -35,7 +35,7 @@ class ChargeNormalizer(BaseChargeNormalizer):
         normalizer = ChargeNormalizer()
         charge = 0
         elem_idxs = torch.tensor([[0, 0, 0, 1, 1]], dtype=torch.long)
-        raw_charges = torch.tensor([[0.3, 0.5, -0.5]], dtype=torch.float)
+        raw_charges = torch.tensor([[0.3, 0.5, -0.5]])
         norm_charges = normalizer(elem_idxs, raw_charges, charge)
         # norm_charges will sum to zero
     """
@@ -57,9 +57,7 @@ class ChargeNormalizer(BaseChargeNormalizer):
             weights = [1.0] * len(symbols)
 
         self.register_buffer(
-            "weights",
-            torch.tensor(weights, dtype=torch.float, device=torch.device("cpu")),
-            persistent=False,
+            "weights", torch.tensor(weights, dtype=torch.float), persistent=False
         )
         self.scale_weights_by_charges_squared = scale_weights_by_charges_squared
 

torchani/neighbors.py
@@ -127,10 +127,10 @@ def compute_bounding_cell(
     largest_dist = max_ - min_
     if square:
         cell = (
-            torch.eye(3, dtype=torch.float, device=coords.device) * largest_dist.max()
+            torch.eye(3, dtype=coords.dtype, device=coords.device) * largest_dist.max()
         )
     else:
-        cell = torch.eye(3, dtype=torch.float, device=coords.device) * largest_dist
+        cell = torch.eye(3, dtype=coords.dtype, device=coords.device) * largest_dist
     if displace:
         # Benchmarks show that these assert coords > 0.0 slows things down a bit
         return coords - min_, cell
@@ -644,7 +644,7 @@ def setup_grid(
     # it is only a lower bound used to calculate the grid size, it is the minimum
     # size that spawns a new bucket in the grid.
     spherical_factor = torch.tensor(
-        [1.0, 1.0, 1.0], dtype=torch.float, device=cell.device
+        [1.0, 1.0, 1.0], dtype=cell.dtype, device=cell.device
     )  # TODO: calculate correctly
     bucket_length_lower_bound = (
         spherical_factor * cutoff / buckets_per_cutoff

torchani/nn/_core.py
@@ -24,6 +24,7 @@ class AtomicOneHot(_Embedding):
         # encoded == torch.tensor([[0, 1, 0], [1, 0, 0], [0, 0, 1], [0, 0, 0]])
 
     """
+
     def __init__(self, symbols: tp.Sequence[str]) -> None:
         super().__init__(symbols)
         num = len(self.symbols)
@@ -49,6 +50,7 @@ class AtomicEmbedding(_Embedding):
         # `encoded` depends on the random init, but it could be for instance:
         # torch.tensor([[1.2, .1], [-.5, .8], [.3, -.4], [0, 0]])
     """
+
     def __init__(self, symbols: tp.Sequence[str], dim: int = 10) -> None:
         super().__init__(symbols)
         num = len(self.symbols)
@@ -126,7 +128,7 @@ class AtomicNetwork(torch.nn.Module):
         dims = tuple(layer_dims)
         self.layers = torch.nn.ModuleList(
             [
-                torch.nn.Linear(_in, _out, bias=bias, dtype=torch.float)
+                torch.nn.Linear(_in, _out, bias=bias)
                 for _in, _out in zip(dims[:-2], dims[1:-1])
             ]
         )
@@ -160,6 +162,7 @@ class AtomicNetwork(torch.nn.Module):
 
 class TightCELU(torch.nn.Module):
     r"""CELU activation function with alpha=0.1"""
+
     def forward(self, x: Tensor) -> Tensor:
         return torch.nn.functional.celu(x, alpha=0.1)
 